59a42c229c3ab8bb9e322584ba50b11f0e233831
Rajan804 <157277476+Rajan804@users.noreply.github.com>
2024-12-01 22:18:38 +0530

    This is my GetX First Tutorial

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:first_tutorial1/homeScreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Homescreen(),
+    );
+  }
+}
